cfe12f982847240077027cbd6cb68e8a2f4071a3
islam18060 <iibrahimov18060@ada.edu.az>
2022-11-26 02:11:24 +0400

    not finished yet

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ function focusImg(imgs) {
     var expandImg = document.getElementById("focusedimg");
     expandImg.src = imgs.src;
     expandImg.parentElement.style.display = "block";
-    alert(`clicked image ${imgs.alt}`)
 }
